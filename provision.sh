d330deb70f9a02b9c8be686d0609b1f6c7257fd0
Casey Milstead <casey@caseymilstead.com>
2017-12-21 15:34:25 -0600

    cleaned up provision script

--- provision.sh
+++ provision.sh
@@ -6,10 +6,11 @@ mysql --login-path=local -e "CREATE DATABASE wordpress"
 mysql --login-path=local wordpress < /var/www/bowtie-wordpress.sql
 rm -f /var/www/bowtie-wordpress.sql
 echo "Generating self-signed SSL certificate"
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048  -subj "/CN=$1.localhost" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
-echo "Generating strong Diffie-Hellman group"
-sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048  -subj "/CN=$1.localhost" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt  > /dev/null 2>&1
+echo "Generating strong Diffie-Hellman group parameters. This will take a while..."
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048 > /dev/null 2>&1
 echo "Restarting NGINX"
 sudo systemctl restart nginx
 echo "🎉  Now serving Wordpress on $1.localhost"
+echo "🔒  HTTPS is available"
 echo "🗄  Go to :8080 to manage the DB"
